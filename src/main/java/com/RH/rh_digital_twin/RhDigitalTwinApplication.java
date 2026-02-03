1f0ae45b7e437b193985740cb91180ee4cc9a7e4
BIBARI Rayen <rayen.bibari@soprahr.com>
2026-02-03 11:42:10 +0100

    Initial commit

---
+++ src/main/java/com/RH/rh_digital_twin/RhDigitalTwinApplication.java
@@ -0,0 +1,13 @@
+package com.RH.rh_digital_twin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhDigitalTwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhDigitalTwinApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/RH/rh_digital_twin/model/Employee.java
@@ -0,0 +1,19 @@
+package com.RH.rh_digital_twin.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String position;
+    private Double salary;
+}
